af09dc1f5eb35717c7d1928b3db1c1b1235803b8
Nikhil Woodruff <nikhil.woodruff@cabinetoffice.gov.uk>
2026-03-30 11:44:32 +0100

    Add towncrier versioning automation
    Changelog fragments in changelog.d/ trigger version bump, changelog
    build, and PyPI publish via GitHub Actions.

---
+++ .github/publish-git-tag.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+VERSION=$(python -c "import re; print(re.search(r'version = \"(.+?)\"', open('pyproject.toml').read()).group(1))")
+git tag "$VERSION"
+git push --tags || true
